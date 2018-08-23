ed8300f3ea0e26aa717c8b69824cb5b88ac1c6cb
Numan Elahi <numan@numans-macbook-pro.local>
2018-08-23 17:00:41 +0530

    :rotating_light: Adds tests

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
+const should = require('should');
 const norm = require('./parser');
 
 // Dummy data
-const data1 = {
+let form_data = {
     "status": ["active"],
     "configuration[icon]": ["icon"],
     "configuration[position]": ["Bottom Left"],
@@ -21,7 +22,29 @@ const data1 = {
     "acknowledgement[type][object2][prop2]": ["Comoestas"],
 }
 
-// TODO: Write proper test for the dummy data above
-const app = norm(data1);
-console.log(app);
+describe("form_data#", () => {
+    before(() => {
+        form_data = norm(form_data);
+    });
+
+
+    it(`should have an attribute 'configuration' which should be an object.`, () => {
+        form_data.should.have.a.property('configuration').which.be.an.Object();
+    });
+
+    it(`'configuration' should have attributes 'icon', 'position' and 'message' equal to 'icon', 'Bottom Left', and 'Nice message' respectively.`, () => {
+        form_data.configuration.should.have.properties('icon', 'position', 'message');
+        form_data.configuration.icon.should.be.equal('icon');
+        form_data.configuration.position.should.be.equal('Bottom Left');
+        form_data.configuration.message.should.be.equal('Nice message');
+    });
+
+    it(`'configuration' should have attribute 'page_rules' which should be an array`, () => {
+        form_data.configuration.should.have.property('page_rules').which.is.an.Array();
+    });
+
+    it(`first element of 'page_rules' should be an object with attributes 'rule' and 'url' equal to 'Never show' and '/aboutus' respectively`, () => {
+        form_data.configuration.page_rules[0].should.be.an.Object().which.deepEqual({rule: 'Never show', url: '/aboutus'});
+    });
+});
 
